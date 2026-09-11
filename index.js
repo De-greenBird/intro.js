82e9214f2600778589e8b021f4052d0b0ba54d26
De-green Bird <emmanuelkhamisv@gmail.com>
2026-09-11 04:53:31 +0200

    Add intro.js with name, age, and favorite language

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello world!')
+const name = "Emmanuel Khamis Victor Loya"
+const age = 28
+const favoriteLanguage = " Python"
+
+console.log('Name: ' +  name)
+console.log('age: ' + age)
+console.log('Favorite Programming Language: ' + favoriteLanguage)
